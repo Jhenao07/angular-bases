9939364087a4ccd696c21a1be28e94d00997b045
JorgeHenao <jorgehenao117@gmail.com>
2024-12-20 17:58:54 -0500

    Guardar cambios antes de actualizar dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { EventEmitter, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -15,6 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
